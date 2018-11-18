2c66454bc297babccf1815eb850405eb255f8144
Sin <dev@zouxin.net>
2018-11-18 23:24:20 +0800

    feat(index): font

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,7 +8,7 @@ fi
 DIR=$(dirname $(dirname $($READLINK -f $0)))
 if [ -z $DIR ]; then exit 1; fi;
 rm -fr $DIR/dist
-git clone --branch=gh-pages --depth=1 git@github.com:ipy/test.git $DIR/dist
+git clone --branch=gh-pages --depth=1 $DEPLOYMENT_REPO $DIR/dist
 rm -fr $DIR/dist/*
 cp -r $DIR/src/* $DIR/dist
 VERSION=`curl -is "https://github.com/chiflix/splayerx/releases/latest" | grep -E '^Location: ' | grep -oE "([0-9]+.)+[0-9]"`
