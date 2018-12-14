987db16d4279b8afcb7c046479f441d986a479b4
Sin <dev@zouxin.net>
2018-12-14 23:39:06 +0800

    try to use gsutil to upload

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,12 +8,20 @@ fi
 DIR=$(dirname $(dirname $($READLINK -f $0)))
 if [ -z $DIR ]; then exit 1; fi;
 rm -fr $DIR/dist
-git clone --branch=gh-pages --depth=1 git@github.com:$TRAVIS_REPO_SLUG.git $DIR/dist
+git clone --branch=gh-pages --depth=1 git@github.com:${TRAVIS_REPO_SLUG:=beta.splayer.org}.git $DIR/dist
 if [ -z $DIR/dist ]; then exit 1; fi;
 rm -fr $DIR/dist/*
 cp -r $DIR/src/* $DIR/dist
 VERSION=`curl -is "https://github.com/chiflix/splayerx/releases/latest" | grep -E '^Location: ' | grep -oE "([0-9]+.)+[0-9]"`
 echo "The latest version is: $VERSION"
+
+openssl aes-256-cbc -K $encrypted_74f063b30305_key -iv $encrypted_74f063b30305_iv -in splayer-cdn-9cc583e96c06.json.enc -out splayer-cdn-9cc583e96c06.json -d
+gcloud auth activate-service-account splayer-release-deployer@splayer-cdn.iam.gserviceaccount.com --key-file=splayer-cdn-9cc583e96c06.json
+DOWNLOAD_URL_DMG="https://github.com/chiflix/splayerx/releases/download/$VERSION/SPlayer-$VERSION.dmg"
+DOWNLOAD_URL_EXE="https://github.com/chiflix/splayerx/releases/download/$VERSION/SPlayer-Setup-$VERSION.exe"
+curl -L "$DOWNLOAD_URL_DMG" | gsutil cp - gs://splayer-releases/SPlayer-$VERSION.dmg
+curl -L "$DOWNLOAD_URL_EXE" | gsutil cp - gs://splayer-releases/SPlayer-Setup-$VERSION.exe
+
 cat $DIR/src/index.html | sed "s/{{version}}/$VERSION/g" > $DIR/dist/index.html
 
 cd $DIR/dist
