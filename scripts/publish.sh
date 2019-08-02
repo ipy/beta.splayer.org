dbe8a75536fa4e4895a0a6dff5cb0996b1b01d90
Zou Xin <dev@zouxin.net>
2019-08-02 12:15:24 +0800

    fix: fix 4.2.0-rc download url

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -18,8 +18,8 @@ echo "The latest version is: $VERSION, published on $DATE"
 
 openssl aes-256-cbc -K $encrypted_74f063b30305_key -iv $encrypted_74f063b30305_iv -in splayer-cdn-9cc583e96c06.json.enc -out splayer-cdn-9cc583e96c06.json -d
 gcloud auth activate-service-account splayer-release-deployer@splayer-cdn.iam.gserviceaccount.com --key-file=splayer-cdn-9cc583e96c06.json
-DOWNLOAD_URL_DMG="https://github.com/chiflix/splayerx/releases/download/$VERSION/SPlayer-$VERSION.dmg"
-DOWNLOAD_URL_EXE="https://github.com/chiflix/splayerx/releases/download/$VERSION/SPlayer-Setup-$VERSION.exe"
+DOWNLOAD_URL_DMG="https://github.com/chiflix/splayerx/releases/download/$VERSION/SPlayer-4.2.0.dmg"
+DOWNLOAD_URL_EXE="https://github.com/chiflix/splayerx/releases/download/$VERSION/SPlayer-Setup-4.2.0.exe"
 curl -L "$DOWNLOAD_URL_DMG" | gsutil cp - gs://splayer-releases/download/SPlayer-$VERSION.dmg
 curl -L "$DOWNLOAD_URL_EXE" | gsutil cp - gs://splayer-releases/download/SPlayer-Setup-$VERSION.exe
 
